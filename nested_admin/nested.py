87870d7fc02e721ea9e9eed8b6bedb038d263f96
Frankie Dintino <fdintino@theatlantic.com>
2012-11-07 20:09:16 -0500

    nesting: Increment javascript version number

--- nested_admin/nested.py
+++ nested_admin/nested.py
@@ -16,7 +16,7 @@ class NestedAdmin(ModelAdmin):
         server_data_js = reverse('nesting_server_data')
         media.add_js((server_data_js,))
 
-        version = 3
+        version = 4
 
         js_files = (
             'jquery.class.js',
