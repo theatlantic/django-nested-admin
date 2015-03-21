e21913bcfdac015a58daccaad33fdbe9333663c4
Frankie Dintino <fdintino@frankiemacbookpro.local>
2015-03-21 00:44:27 -0400

    Add tests; minor tweaks to existing tests

--- nested_admin/tests/test_admin.py
+++ nested_admin/tests/test_admin.py
@@ -6,6 +6,12 @@ from .models import Group, Section, Item
 
 class TestAdmin(BaseNestedAdminTestCase):
 
+    def make_footer_position_static(self):
+        """Make <footer> element styles 'position: static'"""
+        self.selenium.execute_script("document.getElementsByTagName('footer')[0].className = 'grp-module grp-submit-row';")
+        self.selenium.execute_script("if(document.getElementById('content-inner')) {"
+            "document.getElementById('content-inner').style.bottom = '0';}")
+
     def test_add_section_to_empty(self):
         group = Group.objects.create(slug='test')
         self.selenium.get("%s%s" % (self.live_server_url, group.get_absolute_url()))
@@ -50,7 +56,6 @@ class TestAdmin(BaseNestedAdminTestCase):
         group = Group.objects.create(slug='group')
         section_a = Section.objects.create(slug='a', group=group, position=0)
         section_b = Section.objects.create(slug='b', group=group, position=1)
-
         Item.objects.create(name='Item A 0', section=section_a, position=0)
         Item.objects.create(name='Item A 1', section=section_a, position=1)
         Item.objects.create(name='Item A 2', section=section_a, position=2)
@@ -60,10 +65,10 @@ class TestAdmin(BaseNestedAdminTestCase):
 
         self.selenium.get("%s%s" % (self.live_server_url, group.get_absolute_url()))
         self.selenium.set_window_size(1120, 2000)
-        self.selenium.execute_script("document.getElementsByTagName('footer')[0].style.position = 'static';")
+        self.make_footer_position_static()
 
         source = self.selenium.find_element_by_css_selector('#section_set-1-item_set2 > h3')
-        target = self.selenium.find_element_by_css_selector('#section_set-0-item_set1')
+        target = self.selenium.find_element_by_css_selector('#section_set-0-item_set1 > h3')
         ActionChains(self.selenium).drag_and_drop(source, target).perform()
 
         self.selenium.find_element_by_xpath('//input[@name="_continue"]').click()
@@ -97,12 +102,11 @@ class TestAdmin(BaseNestedAdminTestCase):
 
         self.selenium.get("%s%s" % (self.live_server_url, group.get_absolute_url()))
         self.selenium.set_window_size(1120, 2000)
-        self.selenium.execute_script("document.getElementsByTagName('footer')[0].style.position = 'static';")
+        self.make_footer_position_static()
 
-        with self.clickable_xpath('//*[@id="section_set-1-item_set-group"]//a[text()="Add Item"]') as el:
+        with self.clickable_selector('#section_set-1-item_set-group .grp-add-item > a.grp-add-handler.item') as el:
             el.click()
-
-        with self.clickable_xpath('//input[@name="section_set-1-item_set-2-name"]') as el:
+        with self.clickable_selector('#id_section_set-1-item_set-2-name') as el:
             el.send_keys("Item B 2")
 
         source = self.selenium.find_element_by_css_selector('#section_set-1-item_set2 > h3')
@@ -124,6 +128,70 @@ class TestAdmin(BaseNestedAdminTestCase):
             'group/a[0]/Item B 2[1]',
             'group/a[0]/Item A 1[2]',
             'group/a[0]/Item A 2[3]'])
+
+        self.assertEqual(["%s" % i for i in section_b.item_set.all().order_by('position')], [
+            'group/b[1]/Item B 0[0]',
+            'group/b[1]/Item B 1[1]'])
+
+    def test_delete_item(self):
+        group = Group.objects.create(slug='group')
+        section_a = Section.objects.create(slug='a', group=group, position=0)
+        section_b = Section.objects.create(slug='b', group=group, position=1)
+        Item.objects.create(name='Item A 0', section=section_a, position=0)
+        Item.objects.create(name='Item A 1', section=section_a, position=1)
+        Item.objects.create(name='Item A 2', section=section_a, position=2)
+        Item.objects.create(name='Item B 0', section=section_b, position=0)
+        Item.objects.create(name='Item B 1', section=section_b, position=1)
+        Item.objects.create(name='Item B 2', section=section_b, position=2)
+
+        self.selenium.get("%s%s" % (self.live_server_url, group.get_absolute_url()))
+        self.selenium.set_window_size(1120, 2000)
+        self.make_footer_position_static()
+
+        self.selenium.find_element_by_css_selector('#section_set-1-item_set1 a.grp-delete-handler').click()
+
+        self.selenium.find_element_by_xpath('//input[@name="_continue"]').click()
+
+        self.wait_page_loaded()
+
+        self.assertEqual(["%s" % i for i in section_a.item_set.all().order_by('position')], [
+            'group/a[0]/Item A 0[0]',
+            'group/a[0]/Item A 1[1]',
+            'group/a[0]/Item A 2[2]'])
+
+        self.assertEqual(["%s" % i for i in section_b.item_set.all().order_by('position')], [
+            'group/b[1]/Item B 0[0]',
+            'group/b[1]/Item B 2[1]'])
+
+    def test_remove_item(self):
+        group = Group.objects.create(slug='group')
+        section_a = Section.objects.create(slug='a', group=group, position=0)
+        section_b = Section.objects.create(slug='b', group=group, position=1)
+        Item.objects.create(name='Item A 0', section=section_a, position=0)
+        Item.objects.create(name='Item A 1', section=section_a, position=1)
+        Item.objects.create(name='Item A 2', section=section_a, position=2)
+        Item.objects.create(name='Item B 0', section=section_b, position=0)
+        Item.objects.create(name='Item B 1', section=section_b, position=1)
+
+        self.selenium.get("%s%s" % (self.live_server_url, group.get_absolute_url()))
+        self.selenium.set_window_size(1120, 2000)
+        self.make_footer_position_static()
+
+        with self.clickable_selector('#section_set-1-item_set-group .grp-add-item > a.grp-add-handler.item') as el:
+            el.click()
+        with self.clickable_selector('#id_section_set-1-item_set-2-name') as el:
+            el.send_keys("Item B 2")
+
+        self.selenium.find_element_by_css_selector('#section_set-1-item_set2 a.grp-remove-handler').click()
+
+        self.selenium.find_element_by_xpath('//input[@name="_continue"]').click()
+
+        self.wait_page_loaded()
+
+        self.assertEqual(["%s" % i for i in section_a.item_set.all().order_by('position')], [
+            'group/a[0]/Item A 0[0]',
+            'group/a[0]/Item A 1[1]',
+            'group/a[0]/Item A 2[2]'])
 
         self.assertEqual(["%s" % i for i in section_b.item_set.all().order_by('position')], [
             'group/b[1]/Item B 0[0]',
