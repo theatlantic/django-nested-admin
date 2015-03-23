9c839280a31687495081fe15002a7b61b7f969e3
Frankie Dintino <fdintino@theatlantic.com>
2015-03-23 16:03:03 -0400

    Add tests for dragging to first and last position

--- nested_admin/tests/test_admin.py
+++ nested_admin/tests/test_admin.py
@@ -228,3 +228,83 @@ class TestAdmin(BaseNestedAdminTestCase):
         self.assertEqual(["%s" % i for i in section_b.item_set.all().order_by('position')], [
             'group/b[1]/Item B 0[0]',
             'group/b[1]/Item B 1[1]'])
+
+    def test_drag_item_to_first_position(self):
+        group = Group.objects.create(slug='group')
+        section_a = Section.objects.create(slug='a', group=group, position=0)
+        section_b = Section.objects.create(slug='b', group=group, position=1)
+        Item.objects.create(name='Item A 0', section=section_a, position=0)
+        Item.objects.create(name='Item A 1', section=section_a, position=1)
+        Item.objects.create(name='Item A 2', section=section_a, position=2)
+        Item.objects.create(name='Item B 0', section=section_b, position=0)
+        Item.objects.create(name='Item B 1', section=section_b, position=1)
+        Item.objects.create(name='Item B 2', section=section_b, position=2)
+
+        self.selenium.get("%s%s" % (self.live_server_url, group.get_absolute_url()))
+        self.selenium.set_window_size(1120, 2000)
+        self.make_footer_position_static()
+
+        source = self.selenium.find_element_by_css_selector('#section_set-1-item_set2 > h3')
+        target = self.selenium.find_element_by_css_selector('#section_set-0-item_set0')
+        ActionChains(self.selenium).drag_and_drop(source, target).perform()
+
+        self.selenium.find_element_by_xpath('//input[@name="_continue"]').click()
+
+        self.wait_page_loaded()
+
+        item_b_2 = Item.objects.get(name='Item B 2')
+        self.assertEqual(item_b_2.section, section_a, "item was not moved to the correct section")
+        self.assertEqual(item_b_2.position, 0, "item was not moved to the correct position")
+
+        self.assertEqual(["%s" % i for i in section_a.item_set.all().order_by('position')], [
+            'group/a[0]/Item B 2[0]',
+            'group/a[0]/Item A 0[1]',
+            'group/a[0]/Item A 1[2]',
+            'group/a[0]/Item A 2[3]'])
+
+        self.assertEqual(["%s" % i for i in section_b.item_set.all().order_by('position')], [
+            'group/b[1]/Item B 0[0]',
+            'group/b[1]/Item B 1[1]'])
+
+    def test_drag_item_to_last_position(self):
+        group = Group.objects.create(slug='group')
+        section_a = Section.objects.create(slug='a', group=group, position=0)
+        section_b = Section.objects.create(slug='b', group=group, position=1)
+        Item.objects.create(name='Item A 0', section=section_a, position=0)
+        Item.objects.create(name='Item A 1', section=section_a, position=1)
+        Item.objects.create(name='Item A 2', section=section_a, position=2)
+        Item.objects.create(name='Item B 0', section=section_b, position=0)
+        Item.objects.create(name='Item B 1', section=section_b, position=1)
+        Item.objects.create(name='Item B 2', section=section_b, position=2)
+
+        self.selenium.get("%s%s" % (self.live_server_url, group.get_absolute_url()))
+        self.selenium.set_window_size(1120, 2000)
+        self.make_footer_position_static()
+
+        source = self.selenium.find_element_by_css_selector('#section_set-1-item_set2 > h3')
+        target = self.selenium.find_element_by_css_selector('#section_set-0-item_set2')
+
+        (ActionChains(self.selenium)
+            .click_and_hold(source)
+            .move_to_element_with_offset(target, 0, 0)
+            .move_by_offset(0, 200)
+            .release()
+            .perform())
+
+        self.selenium.find_element_by_xpath('//input[@name="_continue"]').click()
+
+        self.wait_page_loaded()
+
+        item_b_2 = Item.objects.get(name='Item B 2')
+        self.assertEqual(item_b_2.section, section_a, "item was not moved to the correct section")
+        self.assertEqual(item_b_2.position, 3, "item was not moved to the correct position")
+
+        self.assertEqual(["%s" % i for i in section_a.item_set.all().order_by('position')], [
+            'group/a[0]/Item A 0[0]',
+            'group/a[0]/Item A 1[1]',
+            'group/a[0]/Item A 2[2]',
+            'group/a[0]/Item B 2[3]'])
+
+        self.assertEqual(["%s" % i for i in section_b.item_set.all().order_by('position')], [
+            'group/b[1]/Item B 0[0]',
+            'group/b[1]/Item B 1[1]'])
