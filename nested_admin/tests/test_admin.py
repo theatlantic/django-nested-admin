04e7ef3762b9152cd3cee157ad6c986d1a02f4bc
Frankie Dintino <fdintino@theatlantic.com>
2015-03-29 16:01:16 -0400

    Don't undelete already deleted nested forms; add test

--- nested_admin/tests/test_admin.py
+++ nested_admin/tests/test_admin.py
@@ -314,6 +314,46 @@ class TestAdmin(BaseNestedAdminTestCase):
             'group/b[0]/Item B 1[1]',
             'group/b[0]/Item B 2[2]'])
 
+    def test_delete_item_undelete_section(self):
+        """
+        Test that, if an item is deleted, then the section is deleted, and
+        then the section is undeleted, that the item stays deleted.
+        """
+        group = Group.objects.create(slug='group')
+        section_a = Section.objects.create(slug='a', group=group, position=0)
+        section_b = Section.objects.create(slug='b', group=group, position=1)
+        Item.objects.create(name='Item A 0', section=section_a, position=0)
+        Item.objects.create(name='Item A 1', section=section_a, position=1)
+        Item.objects.create(name='Item A 2', section=section_a, position=2)
+        Item.objects.create(name='Item B 0', section=section_b, position=0)
+        Item.objects.create(name='Item B 1', section=section_b, position=1)
+        Item.objects.create(name='Item B 2', section=section_b, position=2)
+
+        self.selenium.get("%s%s" % (self.live_server_url, group.get_absolute_url()))
+        self.selenium.set_window_size(1120, 2000)
+        self.make_footer_position_static()
+
+        self.selenium.find_element_by_css_selector('#section_set-0-item_set1 a.grp-delete-handler').click()
+        self.selenium.find_element_by_css_selector('#section_set0 a.grp-delete-handler.section').click()
+        self.wait_until_clickable_selector('#section_set0.grp-predelete a.grp-delete-handler.section')
+        self.selenium.find_element_by_css_selector('#section_set0 a.grp-delete-handler.section').click()
+        self.wait_until_clickable_selector('#section_set0:not(.grp-predelete) a.grp-delete-handler.section')
+
+        self.selenium.find_element_by_xpath('//input[@name="_continue"]').click()
+
+        self.wait_page_loaded()
+
+        self.assertEqual(len(Section.objects.filter(slug='a')), 1, "Section should not be deleted")
+
+        self.assertEqual(["%s" % i for i in section_a.item_set.all().order_by('position')], [
+            'group/a[0]/Item A 0[0]',
+            'group/a[0]/Item A 2[1]'])
+
+        self.assertEqual(["%s" % i for i in section_b.item_set.all().order_by('position')], [
+            'group/b[1]/Item B 0[0]',
+            'group/b[1]/Item B 1[1]',
+            'group/b[1]/Item B 2[2]'])
+
     def test_remove_item(self):
         group = Group.objects.create(slug='group')
         section_a = Section.objects.create(slug='a', group=group, position=0)
