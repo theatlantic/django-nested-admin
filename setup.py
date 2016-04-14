63aa850cfd564dce88b0c18e0b9e76038eb67881
Frankie Dintino <fdintino@theatlantic.com>
2016-04-14 00:00:40 -0400

    v3.0 release! [ci skip]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='django-nested-admin',
-    version="3.0.0a1",
+    version="3.0.0",
     install_requires=[
         'python-monkey-business>=1.0.0',
     ],
