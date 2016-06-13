c4a4b4aa631e4bdc9b0d72ba81f6bb7de33e96fd
Frankie Dintino <fdintino@theatlantic.com>
2016-06-13 19:08:36 -0400

    Fix python 3 bug

--- nested_admin/tests/admin_widgets/tests.py
+++ nested_admin/tests/admin_widgets/tests.py
@@ -1,7 +1,5 @@
-import sys
 import time
 from unittest import expectedFailure
-from unittest.case import _ExpectedFailure, _UnexpectedSuccess
 
 from django.utils import six
 from django.utils.text import slugify, unescape_entities
@@ -143,25 +141,16 @@ class TestAdminWidgets(BaseNestedAdminTestCase):
         self.add_inline()
         self.check_m2m([1])
 
+    @expected_failure_if_suit  # Known bug with this test, django-suit, and phantomjs
     def test_add_fk(self):
-        # Known bug with this test, django-suit, and phantomjs
-        expected_failure = self.has_suit and self.browser == 'phantomjs'
-        try:
-            self.load_admin()
-            if self.has_grappelli:
-                time.sleep(0.3)
-            self.add_inline()
-            if self.has_grappelli:
-                time.sleep(0.3)
-            time.sleep(0.1)
-            self.check_fk([1])
-        except:
-            if expected_failure:
-                raise _ExpectedFailure(sys.exc_info())
-            raise
-        else:
-            if expected_failure:
-                raise _UnexpectedSuccess
+        self.load_admin()
+        if self.has_grappelli:
+            time.sleep(0.3)
+        self.add_inline()
+        if self.has_grappelli:
+            time.sleep(0.3)
+        time.sleep(0.1)
+        self.check_fk([1])
 
     @expectedFailure  # Known bug
     def test_add_initial_extra_m2m(self):
