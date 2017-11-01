3a75b938ca0b802c180df822d2cdc5e58b321aa1
Frankie Dintino <fdintino@gmail.com>
2017-11-01 17:13:23 -0400

    v3.0.21 [ci skip]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='django-nested-admin',
-    version="3.0.20",
+    version="3.0.21",
     install_requires=[
         'python-monkey-business>=1.0.0',
     ],
