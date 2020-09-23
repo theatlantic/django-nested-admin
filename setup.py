0b1dff91d36f249e189dac939314ee9655fe6bf3
rtconverter <rtconverter@gmail.com>
2020-09-23 17:00:15 +0300

    changed double quotes to single quotes

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
